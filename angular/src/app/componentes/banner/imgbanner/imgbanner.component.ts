f1a7ea862a3b61fb3c58f983e9e30b5b1821ef5e
cintiasana <cintiaelisana@gmail.com>
2022-12-21 20:09:45 -0300

    servicio:banner-imgBanner

--- angular/src/app/componentes/banner/imgbanner/imgbanner.component.ts
+++ angular/src/app/componentes/banner/imgbanner/imgbanner.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatosService } from '../../servicios/datos.service';
 
 @Component({
   selector: 'app-imgbanner',
   templateUrl: './imgbanner.component.html',
   styleUrls: ['./imgbanner.component.css']
 })
-export class ImgbannerComponent {
+export class ImgbannerComponent implements OnInit{
+  imgBanner: string='';
+
+  constructor (private datos:DatosService){}
+  ngOnInit(): void {
+    this.datos.obtenerDatos().subscribe(data =>{
+      this.imgBanner = data.imgBanner;
+    })
+    
+  }
 
 }
