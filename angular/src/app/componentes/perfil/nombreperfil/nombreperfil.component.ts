eb8c5b0ead430361e47f90e331979ef7fdb80ae2
cintiasana <cintiaelisana@gmail.com>
2022-12-22 16:50:07 -0300

    servicio:perfil-nombrePerfil

--- angular/src/app/componentes/perfil/nombreperfil/nombreperfil.component.ts
+++ angular/src/app/componentes/perfil/nombreperfil/nombreperfil.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatosService } from '../../servicios/datos.service';
 
 @Component({
   selector: 'app-nombreperfil',
   templateUrl: './nombreperfil.component.html',
   styleUrls: ['./nombreperfil.component.css']
 })
-export class NombreperfilComponent {
+export class NombreperfilComponent implements OnInit {
+  nombrePerfil: string='';
+
+  constructor(private datos:DatosService){}
+
+  ngOnInit(): void {
+    this.datos.obtenerDatos().subscribe(data =>{
+      this.nombrePerfil = data.nombrePerfil;
+    })
+    
+  }
 
 }
