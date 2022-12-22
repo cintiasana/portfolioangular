004b7c3c77363e80bb8269f9293c874cf66b4a3e
cintiasana <cintiaelisana@gmail.com>
2022-12-22 17:34:41 -0300

    servicios:experiencia--falta eliminar componentes sobrantes

--- angular/src/app/componentes/experiencia/experiencia.component.ts
+++ angular/src/app/componentes/experiencia/experiencia.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatosService } from '../servicios/datos.service';
 
 @Component({
   selector: 'app-experiencia',
   templateUrl: './experiencia.component.html',
   styleUrls: ['./experiencia.component.css']
 })
-export class ExperienciaComponent {
+export class ExperienciaComponent implements OnInit {
+  experiencias: any = [];
+
+  constructor(private datos:DatosService){}
+
+  ngOnInit(): void {
+    this.datos.obtenerDatos().subscribe(data => {
+      this.experiencias = data.experiencias;
+    })
+  }
 
 }
